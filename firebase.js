fbde7862bcade5bf6129363561dd0eb3c3ce95ee
Seung Hyun Han <136709860+tmdgus1dnl@users.noreply.github.com>
2025-09-19 11:07:27 +0900

    Delete firebase.js

--- firebase.js
+++
@@ -1,9 +0,0 @@
-// Your web app's Firebase configuration
-export const firebaseConfig = {
-  apiKey: "AIzaSyDs4i16Vp3h6cc8rIlN_ShdjWAq_qK50C0",
-  authDomain: "ssafy-pjt-b90b2.firebaseapp.com",
-  projectId: "ssafy-pjt-b90b2",
-  storageBucket: "ssafy-pjt-b90b2.firebasestorage.app",
-  messagingSenderId: "910313317735",
-  appId: "1:910313317735:web:03dd07f958af085727bbc2"
-};
